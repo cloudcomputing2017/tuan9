87ec8f876a90092b32c5d9b0214bf9f1c5f78bcf
Thốt Nốt <thuhangpt96@hotmail.com>
2017-04-15 11:11:42 +0700

    Update post.sql

--- post.sql
+++ post.sql
@@ -1,10 +1,7 @@
 create database postSample;
-use postSample
-
-
-use postSample 
+use postSample;
 create table post(
  id int(11) NOT NULL AUTO_INCREMENT primary key,
  title nvarchar(100),
- content nvarchar(300)
+ content longtext
 );
